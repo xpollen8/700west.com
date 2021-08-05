c32b086d0bc1fdfa04619adf43a5831fe89f0054
David Whittemore <github@xpollen8.com>
2021-08-05 11:24:05 -0700

    dw: let next handle /audio /image redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ module.exports = {
   async redirects() {
 		return [
 			{
+				source: '/audio/:path*',
+				destination: `${process.env.NEXT_PUBLIC_AUDIO}/:path*`,
+				permanent: false,
+			},
+			/* USE IF IMAGES ARE EVER SERVED NON_LOCALLY
+			{
+				source: '/images/:path*',
+				destination: `${process.env.NEXT_PUBLIC_IMAGES}/:path*`,
+				permanent: false,
+			},
+			*/
+			{
 				source: '/htdb/releases/singles.html',
 				destination: '/singles',
 				permanent: false,
