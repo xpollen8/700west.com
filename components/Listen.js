c8bcbf636c1b9491bd627b6002444aad96f52e1e
David Whittemore <github@xpollen8.com>
2021-08-08 14:25:27 -0700

    dw: more futz

--- components/Listen.js
+++ components/Listen.js
@@ -8,13 +8,13 @@ const makeListenAlbum = ({ artist, title, tracks }, key) => (
 		<span>
 			- <a href={makeReleaseLink(artist, title)}>{title}</a>
 		</span>
-		<div className="player">
-		{tracks.filter(t => t.audio.length > 0).map(( data, key) => (
-			<div key={key} className="row">
-				{AudioPlayer(data)}
-			</div>
+		<blockquote>
+			{tracks.filter(t => t.audio.length > 0).map(( data, key) => (
+				<div key={key} className="row">
+					{AudioPlayer(data)}
+				</div>
 			))}
-		</div>
+		</blockquote>
 	</div>
 )
 
@@ -33,12 +33,12 @@ const Listen = (props) => (
 
 	<SectionHeader text="Interviews" />
 		<span className="artist">Dan Modlin interviews Moe Whittemore <span className="date ago">2010</span></span>
-		<div className="player row">
+		<blockquote className="row">
 			<AudioPlayer mp3='2010_DanModlin_700West_Interview.mp3'
 				title='Interview'
-				time='12.51'
+				time='13:29'
 				/>
-		</div>
+		</blockquote>
 
 	<SectionHeader text="L.P. Tracks" />
 		<>
@@ -51,76 +51,80 @@ const Listen = (props) => (
 		</>
 
 	<SectionHeader text="Unreleased Material" />
-	<ol>
-		<li><AudioPlayer mp3='Mo_WaitTilYesterday.mp3'
+	<blockquote>
+		<p className="row"><AudioPlayer mp3='Mo_WaitTilYesterday.mp3'
 			band='LCD (Least Common Denominator)'
 			title='Wait Til Yesterday' time='3:25'
 			comment='Unreleased Mo demo' date='1977' />
-		</li>
-		<li><AudioPlayer mp3='UltimateForce/CircleCityFatback.mp3'
+		</p>
+		<p className="row"><AudioPlayer mp3='UltimateForce/CircleCityFatback.mp3'
 			band='Ultimate Force'
 			title='Circle City Fatback'
 			comment="Smokin' funk!, taken from submasters" />
-		</li>
-		<li><AudioPlayer mp3='Jeannie.mp3'
+		</p>
+		<p className="row"><AudioPlayer mp3='Jeannie.mp3'
 			band='Herman Walker'
 			title='Jeannie' time='2:45'
 			comment="The proper interpretation of one Jerome Winoker's unrequited love songs.
 			Larry Lucas: Guitar, Herman Walker: Vocals, Bo Gooliak: Bass, Mel Cupp: Drums" />
-		</li>
-		<li><AudioPlayer mp3='FunkStWorkshop/Water.mp3'
+		</p>
+		<p className="row"><AudioPlayer mp3='FunkStWorkshop/Water.mp3'
 			band='Funk St. Workshop'
 			title='Water'
 			comment='Taken from submasters' />
-		</li>
-		<li><AudioPlayer mp3='Jubal/CertainKindOfLady.mp3'
+		</p>
+		<p className="row"><AudioPlayer mp3='Jubal/CertainKindOfLady.mp3'
 			band='Jubal (Zerfas)'
 			title='A Certain Kind Of Lady'
 			comment='Unreleased demo'
 			date='1979' />
-		</li>
-		<li><AudioPlayer mp3='Jubal/ShakeUpYourMind.mp3'
+		</p>
+		<p className="row"><AudioPlayer mp3='Jubal/ShakeUpYourMind.mp3'
 			band='Jubal (Zerfas)'
 			title='Shake Up Your Mind'
 			comment='Unreleased demo, taken from submasters'
 			date='1979' />
-		</li>
-	</ol>
+		</p>
+	</blockquote>
 
 	<SectionHeader text="M. J. Whittemore, Jr.'s Classical Compositions" />
-	<p>
+	<blockquote>
 		In 2003, Moe Whittemore returned to graduate school at Butler University
 		to complete the music degree he had started back in the 50's!
 		Here are his MIDI representations of the classical composition class assigments
 		- on which he received all A's
-	</p>
-	<ol>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/MarchMadness.mp3' title='March Madness'
-			date='April 2003' /></li>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/InQuadrature.mp3' title='In Quadrature'
-			date='July 2003' /></li>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/2-PartInvention.mp3' title='2-Part Invention'
-			date='November 2003' /></li>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/3-VoiceFugue.mp3' title='3-Voice Fugue'
-			date='December 2003' /></li>
-	</ol>
+	</blockquote>
+	<blockquote>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/MarchMadness.mp3' title='March Madness'
+			date='April 2003' /></p>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/InQuadrature.mp3' title='In Quadrature'
+			date='July 2003' /></p>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/2-PartInvention.mp3' title='2-Part Invention'
+			date='November 2003' /></p>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/3-VoiceFugue.mp3' title='3-Voice Fugue'
+			date='December 2003' /></p>
+	</blockquote>
+
 	<p style={{ textAlign: 'center' }}>
 		<Image src={`/images/Moconcert3.jpg`} width="288" height="432" />
-		<br/>
-		<b>Wright Now!</b> (November 2003)
 	</p>
-	<ol>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_01TheWinding.mp3' title='The Winding' /></li>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_02_TheClimb.mp3' title='The Climb' /></li>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_03_BumpingAbout.mp3' title='Bumping About' /></li>
-		<li><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_04_Descent.mp3' title='Descent' /></li>
-	</ol>
-	<p>
-	<AudioPlayer mp3='ButlerStudentVolumeOne/Our4FeralKittens.mp3' title='Our 4 Feral Kittens'
-		time="4:52"
-		comment='Not really a classical composition...'
-		date='December 2003' />
-	</p>
+	<b>Wright Now!</b> (November 2003)
+	<blockquote>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_01TheWinding.mp3' title='The Winding' /></p>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_02_TheClimb.mp3' title='The Climb' /></p>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_03_BumpingAbout.mp3' title='Bumping About' /></p>
+		<p className="row"><AudioPlayer mp3='ButlerStudentVolumeOne/WrightNow_04_Descent.mp3' title='Descent' /></p>
+	</blockquote>
+
+	<b>More Compositions</b>
+	<blockquote>
+		<p className="row">
+		<AudioPlayer mp3='ButlerStudentVolumeOne/Our4FeralKittens.mp3' title='Our 4 Feral Kittens'
+			time="4:52"
+			comment='Not really a classical composition...'
+			date='December 2003' />
+		</p>
+	</blockquote>
 	</>
 )
 
