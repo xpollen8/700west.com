0a8d64b012e14903ae4af50de62b7825aef2de44
David Whittemore <github@xpollen8.com>
2021-09-01 18:51:41 -0700

    dw: cleanup

--- pages/contact.js
+++ pages/contact.js
@@ -2,18 +2,16 @@ import { Page } from './_app';
 
 const App = (props) => (
 	<Page title="Contact" link="contact" description="We'd love to hear from you!">
+	<p className="panelContainer">
 	<p>
 		If you have any material (audio, albums scans, press clippings) related to any of the bands associated with the 700 West Studio, we've got a home for it... right here!
 	</p>
 	<p>
-		We are trying to keep in touch with musicians and visitors who had anything to do with 700 West Studio. Catch up on old times, come get your master tapes!
+		Share memories about time spent at the studio. We'd love to hear from you. We are actively soliciting material - stories, scans, whatever!
+	</p>
 	</p>
 	<p>
-		Help fill out your album's information with your memories about the studio. We'd love to hear from you. We are actively soliciting material - stories, scans, whatever!
-	</p>
-	<p>
-		Get in touch with us at: contact[at]700west.com
-		(Be sure to change the "[at] to "@" - we're fighting robots, here!)
+		Get in touch at: contact[at]700west.com
 	</p>
 	</Page>
 )
