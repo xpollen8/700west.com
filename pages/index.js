29c79df1f72e233ecd5fbbf0612c98353a779f51
David Whittemore <github@xpollen8.com>
2021-10-31 11:12:00 -0700

    dw: added 19730205_Primevil_Zerfas.jpg to homepage rotation

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ import News from '../components/News';
 import History from '../components/History';
 
 const	ImageRotate = () => {
-	switch (Math.floor(Math.random() * 3)) {
+	switch (Math.floor(Math.random() * 4)) {
 		case 1: return { src: `/images/250_business_card.jpg`, width: 250, height: 147, alt: "Business Card" }
 		case 2: return { src: `/images/72adb.gif`, width: 379, height: 192, alt: "Newspaper Ad" }
+		case 3: return { src: `/images/19730205_Primevil_Zerfas.jpg `, width: 250, height: 215, alt: "Big Day!" }
 		default: return { src: `/images/newsad.jpg`, width: 367, height: 152, alt: "Newspaper Ad" }
 	}
 }
