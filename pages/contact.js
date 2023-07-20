4c07f3eb587dc2078338fdff34b48519bf4fe904
xpollen8 <github@xpollen8.com>
2023-07-19 22:51:13 -0700

    dw: p->div

--- pages/contact.js
+++ pages/contact.js
@@ -2,17 +2,17 @@ import { Page } from './_app';
 
 const App = (props) => (
 	<Page title="Contact" link="contact" description="We'd love to hear from you!">
-	<p className="panelContainer">
-	<p>
-		If you have any material (audio, albums scans, press clippings) related to any of the bands associated with the 700 West Studio, we've got a home for it... right here!
-	</p>
-	<p>
-		Share memories about time spent at the studio. We'd love to hear from you. We are actively soliciting material - stories, scans, whatever!
-	</p>
-	</p>
-	<p>
-		Get in touch at: contact[at]700west.com
-	</p>
+		<div className="panelContainer">
+			<p>
+				If you have any material (audio, albums scans, press clippings) related to any of the bands associated with the 700 West Studio, we've got a home for it... right here!
+			</p>
+			<p>
+				Share memories about time spent at the studio. We'd love to hear from you. We are actively soliciting material - stories, scans, whatever!
+			</p>
+		</div>
+		<div>
+			Get in touch at: contact[at]700west.com
+		</div>
 	</Page>
 )
 
