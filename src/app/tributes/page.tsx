7bc7b0a22d44fc216756806dd59a637b93fe87c5
xpollen8 <github@xpollen8.com>
2023-08-11 12:00:35 -0700

    dw: desc change

--- src/app/tributes/page.tsx
+++ src/app/tributes/page.tsx
@@ -5,7 +5,7 @@ import Tributes from '../../components/Tributes';
 export const metadata: Metadata = setTitleFromURL('/tributes');
 
 const App = () => 
-<Page link="/tributes" description="Remembrances by recording artists">
+<Page link="/tributes" description="Recording artists share..">
 	<Tributes />
 </Page>
 
