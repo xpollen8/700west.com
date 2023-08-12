2f1141d9d403a4a2a94d820010c9a2c7a102052b
xpollen8 <github@xpollen8.com>
2023-08-11 18:19:55 -0700

    dw: TSify

---
+++ src/app/extras/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+import { Page, setTitleFromURL } from '../layout';
+import Extras from '../../components/Extras';
+
+export const metadata: Metadata = setTitleFromURL('/extras');
+
+const App = () => (
+	<Page title="Extras" link="extras" description="Reviews and Other Auxiliary Materials ">
+		<Extras />
+	</Page>
+);
+
+export default App;
