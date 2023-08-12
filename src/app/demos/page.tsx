fe641af150431132b4f0d79466abf19e920f8c35
xpollen8 <github@xpollen8.com>
2023-08-11 18:11:57 -0700

    dw: TSify

---
+++ src/app/demos/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+import { Page, setTitleFromURL } from '../layout';
+import Demos from '../../components/Demos';
+
+export const metadata: Metadata = setTitleFromURL('/demos');
+
+const App = () => {
+	return <Page title="Releases" link="demos" description="The Demos">
+		<Demos />
+	</Page>
+}
+
+export default App;
