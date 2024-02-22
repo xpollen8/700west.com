11d2b61f57f30762caf94ae13fd08aaf8ad13cf7
xpollen8 <github@xpollen8.com>
2024-02-21 23:08:30 -0800

    dw: more misspellings

--- src/lib/AKAs.js
+++ src/lib/AKAs.js
@@ -1,9 +1,11 @@
 const AKAs = {
+	'Craig Parmerlee': [ 'Graig Parmerlee*' ],
+	'Darrel Watkins': [ 'Darrell Watkins*' ],
 	'Robert Williams': [ 'Bob Williams' ],
 	'Bradford Braden': [ 'B. Braden', 'Brad Braden' ],
 	'Gary Brewer': [ 'Gary Lee Brewer' ],
 	'Jason Seidler': [ 'Jason Stonewall' ],
-	'Maurice James Whittemore, Jr.': [ 'M.J. Whittemore, Jr.', 'Mo', 'Moe', 'Mo Whittemore', 'Moe Whittemore', 'M. Whittemore Jr', 'M. Whittemore', 'Moe Whittimore*', 'M.J. Whittimore, Jr.*', 'M.J. Whitamore*', 'Mo Wittemore*', 'M.J. Whitemore*' ],
+	'Maurice James Whittemore, Jr.': [ 'M.J. Whittemore, Jr.', 'Mo', 'Moe', 'Mo Whittemore', 'Moe Whittemore', 'M. Whittemore Jr', 'M. Whittemore', 'M.J. Whittemore', 'Moe Whittimore*', 'M.J. Whittimore, Jr.*', 'M.J. Whitamore*', 'Mo Wittemore*', 'M.J. Whitemore*' ],
 	'Jay Wilfong': [ 'Jay Wilfong', 'William Bonney' ],
 	'Arthur Swords': [ 'Shanty' ],
 	'Carlos Silva': [ 'Silva' ],
