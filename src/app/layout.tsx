9b025dd11852abb36f016c9527ce738a4630a34c
David Whittemore <github@xpollen8.com>
2025-06-04 12:59:57 -0700

    dw: remove viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import { Metadata } from 'next'
 import '../styles/globals.css'
  
 export const metadata: Metadata = {
+	/*
 	viewport: {
 		initialScale: 1.0,
 		width: 'device-width',
 	},
+	*/
 	description: "Curated archive of Mo WHittemore's 700 West Recording studio",
 	keywords: [
 		"700 West", "Mo Whittemore", "Moe Whittemore", "Zerfas", "Primevil", "Indiana music", "70's & 80's"
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -25,10 +25,12 @@ const links: any  = {
 }
 
 export const setTitle = (title: string): Metadata => ({
+		/*
 		viewport: {
 			initialScale: 1.0,
 			width: 'device-width',
 		},
+		*/
 		description: "Curated archive of Mo WHittemore's 700 West Recording studio",
 		keywords: [
 			"700 West", "Mo Whittemore", "Moe Whittemore", "Zerfas", "Primevil", "Indiana music", "70's & 80's"
