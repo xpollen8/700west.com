91614baa022d28da3d18ded147c703844c24ae09
xpollen8 <github@xpollen8.com>
2025-06-04 20:49:00 -0700

    dw: autolink

--- src/components/MaybeReleaseLink.js
+++ src/components/MaybeReleaseLink.js
@@ -1,7 +1,7 @@
-import { isRelease, makeReleaseLink } from '../lib/helpers';
+import { isRelease, makeReleaseLink, autoLink } from '../lib/helpers';
 
 const MaybeReleaseLink = (artist='', title='') => {
-	if (!isRelease(artist, title)) return <><b>{artist}</b> - {title}</>;
+	if (!isRelease(artist, title)) return <><b>{autoLink(artist)}</b> - {autoLink(title)}</>;
 	return (
 		<a href={`${makeReleaseLink(artist, title)}`}><b>{artist}</b> - {title}</a>
 	)
